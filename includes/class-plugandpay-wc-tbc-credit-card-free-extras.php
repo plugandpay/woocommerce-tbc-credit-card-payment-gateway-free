2c1302c5ad4bf69e728f07583b7d2eda3c59fdce
Sandro Dzneladze <sandrodz@gmail.com>
2019-02-13 22:26:02 +0400

    feat: Product and Blog dashbiard widgets

--- includes/class-plugandpay-wc-tbc-credit-card-free-extras.php
+++ includes/class-plugandpay-wc-tbc-credit-card-free-extras.php
@@ -37,7 +37,8 @@ class PlugandPay_WC_TBC_Credit_Card_Free_Extras {
 	public function __construct( $file ) {
 		$this->file = $file;
 
-		add_filter( 'woocommerce_gateway_icon', array( $this, 'add_gateway_icons' ), 10, 2 );
+		add_filter( 'woocommerce_gateway_icon', [ $this, 'add_gateway_icons' ], 10, 2 );
+		add_action( 'wp_dashboard_setup', [ $this, 'init_dashboard_widgets' ] );
 	}
 
 	/**
@@ -56,6 +57,144 @@ class PlugandPay_WC_TBC_Credit_Card_Free_Extras {
 			);
 		}
 		return $icons;
+	}
+
+	/**
+	 * Init dashboard widgets.
+	 *
+	 * @since 1.0.4
+	 */
+	public function init_dashboard_widgets() {
+
+		wp_add_dashboard_widget(
+			'plugandpay_products_widget',
+			__( 'Plug and Pay - Products', 'tbc-gateway-free' ),
+			[ $this, 'display_products_widget' ]
+		);
+
+		wp_add_dashboard_widget(
+			'plugandpay_blog_widget',
+			__( 'Plug and Pay - Blog', 'tbc-gateway-free' ),
+			[ $this, 'display_blog_widget' ]
+		);
+
+	}
+
+	/**
+	 * Products widget.
+	 *
+	 * @since 1.0.4
+	 */
+	public function display_products_widget() {
+		$this->display_feed( 'https://plugandpay.ge/shop/feed/', [ 'type' => 'products' ] );
+		?>
+
+		<p class="community-events-footer" style="margin:-12px;padding-bottom:0;">
+			<?php
+				printf(
+					'<a href="%1$s" target="_blank">%2$s <span class="screen-reader-text">%3$s</span><span aria-hidden="true" class="dashicons dashicons-external"></span></a>',
+					'https://plugandpay.ge/shop/?utm_source=tbcfree&utm_medium=dashboard&utm_campaign=productsWidget',
+					esc_html__( 'Shop', 'tbc-gateway-free' ),
+					/* translators: accessibility text */
+					esc_html__( '(opens in a new window)', 'tbc-gateway-free' )
+				);
+			?>
+
+		<?php
+	}
+
+	/**
+	 * Blog widget.
+	 *
+	 * @since 1.0.4
+	 */
+	public function display_blog_widget() {
+		$this->display_feed( 'https://plugandpay.ge/feed/' );
+		?>
+
+		<p class="community-events-footer" style="margin:-12px;padding-bottom:0;">
+			<?php
+				printf(
+					'<a href="%1$s" target="_blank">%2$s <span class="screen-reader-text">%3$s</span><span aria-hidden="true" class="dashicons dashicons-external"></span></a>',
+					'https://plugandpay.ge/blog/?utm_source=tbcfree&utm_medium=dashboard&utm_campaign=blogWidget',
+					esc_html__( 'Blog', 'tbc-gateway-free' ),
+					/* translators: accessibility text */
+					esc_html__( '(opens in a new window)', 'tbc-gateway-free' )
+				);
+			?>
+
+		<?php
+	}
+
+	/**
+	 * Display feed.
+	 *
+	 * @since 1.0.4
+	 * @param string $url RSS feed url.
+	 * @param array  $args Optional arguments.
+	 */
+	public function display_feed( $url, $args = [] ) {
+		$feed = fetch_feed( $url );
+
+		if ( ! is_object( $feed ) ) {
+			return;
+		}
+
+		if ( is_wp_error( $feed ) ) {
+			if ( is_admin() || current_user_can( 'manage_options' ) ) {
+				echo '<p><strong>' . esc_html__( 'RSS Error:', 'tbc-gateway-free' ) . '</strong> ' . esc_html( $feed->get_error_message() ) . '</p>';
+				return;
+			}
+		}
+
+		$default_args = [
+			'type'  => 'posts',
+			'items' => 5,
+		];
+		$args         = wp_parse_args( $args, $default_args );
+
+		if ( ! $feed->get_item_quantity() ) {
+			echo '<ul><li>' . esc_html__( 'An error has occurred, which probably means the feed is down. Try again later.', 'tbc-gateway-free' ) . '</li></ul>';
+			$feed->__destruct();
+			unset( $feed );
+			return;
+		}
+
+		echo '<ul>';
+		switch ( $args['type'] ) {
+
+			case 'posts':
+				$campaign = 'utm_source=tbcfree&utm_medium=dashboard&utm_campaign=blogWidget';
+				foreach ( $feed->get_items( 0, $args['items'] ) as $item ) {
+					$image = $item->get_item_tags( '', 'image' );
+					echo sprintf(
+						'<li><img src="%s" style="float:left;width:30%%;padding-right:5%%;" /><div style="float:left;width:65%%;"><a href="%s?%s">%s</a><p>%s</p></div><div style="clear: both;"></div></li>',
+						esc_url( isset( $image[0]['data'] ) ? $image[0]['data'] : '' ),
+						esc_url( $item->get_link() ),
+						esc_html( $campaign ),
+						esc_html( $item->get_title() ),
+						esc_html( wp_trim_words( $item->get_description(), 14, ' [&hellip;]' ) )
+					);
+				}
+				break;
+
+			case 'products':
+				$campaign = 'utm_source=tbcfree&utm_medium=dashboard&utm_campaign=productsWidget';
+				foreach ( $feed->get_items( 0, $args['items'] ) as $item ) {
+					$image = $item->get_item_tags( '', 'image' );
+					echo sprintf(
+						'<li style="display:inline-block;width:33.33%%;"><a href="%s?%s"><img src="%s" style="width:95%%;padding-bottom:5%%;" /></a></li>',
+						esc_url( $item->get_link() ),
+						esc_html( $campaign ),
+						esc_url( isset( $image[0]['data'] ) ? $image[0]['data'] : '' )
+					);
+				}
+				break;
+		}
+		echo '</ul>';
+
+		$feed->__destruct();
+		unset( $feed );
 	}
 
 }
